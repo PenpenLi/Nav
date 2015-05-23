0890ae02604d45e4e058fa9fe555a49ac288f895
lly20000 <lly20000@gmail.com>
2015-05-24 02:25:52 +0800

    Formation match with ship 1st version

--- Assets/Resources/Script/Fight/Team.cs
+++ Assets/Resources/Script/Fight/Team.cs
@@ -9,7 +9,7 @@ public class Team : MonoBehaviour {
     private List<GameObject> m_ShipList = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
-	
+        AddShip(1);
 	}
 	
 	// Update is called once per frame
@@ -17,7 +17,7 @@ public class Team : MonoBehaviour {
 	
 	}
 
-    bool AddShip(int nIndex)
+    public bool AddShip(int nIndex)
     {
         GameObject prefab = Instantiate(Resources.Load("Prefab/Ships/Ship" + Convert.ToString(nIndex)), Vector3.zero, Quaternion.identity) as GameObject;
         if(prefab == null)
@@ -25,9 +25,25 @@ public class Team : MonoBehaviour {
             Debug.Log("Load Prefab/Ships/Ship" + nIndex + "failed");
             return false;
         }
+        m_ShipList.Add(prefab);
+        prefab.transform.parent = TeamSelf.transform;
         prefab.transform.localPosition = Vector3.zero;
         prefab.transform.localScale = Vector3.one;
+        return true;
+    }
+
+    public bool AddShip(int nIndex, Vector3 pos)
+    {
+        GameObject prefab = Instantiate(Resources.Load("Prefab/Ships/Ship" + Convert.ToString(nIndex)), Vector3.zero, Quaternion.identity) as GameObject;
+        if (prefab == null)
+        {
+            Debug.Log("Load Prefab/Ships/Ship" + nIndex + "failed");
+            return false;
+        }
         m_ShipList.Add(prefab);
+        prefab.transform.parent = TeamSelf.transform;
+        prefab.transform.localPosition = Vector3.zero;
+        prefab.transform.localScale = Vector3.one;
         return true;
     }
 }
