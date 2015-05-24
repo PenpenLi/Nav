dc0ea4fd3a4912648ac1f4e523fe9e0da7076e50
lly20000 <lly20000@gmail.com>
2015-05-24 15:59:50 +0800

    add forgot files

---
+++ Assets/Resources/Script/Fight/Fight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fight : MonoBehaviour {
+    public GameObject m_Team1;
+    public GameObject m_Team2;
+    public GameObject m_AttFormation;
+    public GameObject m_DefFormation;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //加载指定阵型
+    bool LoadFormation(int Index, int type)
+    {
+
+        return true;
+    }
+}
---
+++ Assets/Script/Formation/Formation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Formation : MonoBehaviour {
+    public List<GameObject> m_PosList = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+        Vector3 vec3 = GetIndexPos(0);
+        Debug.Log("vec3 = " + vec3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //返回指定索引的船体位置 1-5进攻方 6-10防守方（做这个Prefab必须要保证进攻方的位置和防守方的位置要做到镜像一样）
+    public Vector3 GetIndexPos(int Index)
+    {
+        if (Index > 10 || Index < 1)
+        {
+            return Vector3.zero;
+        }
+        for (int i = 0; i < m_PosList.Count; i++)
+        {
+            Debug.Log("m_PosList[i].transform.localPosition = " + m_PosList[i].transform.localPosition);
+            if (Index == i)
+            {
+                Vector3 pos = m_PosList[i].transform.localPosition;
+                //Debug.Log("pos = " + pos);
+                return pos;
+            }
+        }
+        return Vector3.zero;
+    }
+}
