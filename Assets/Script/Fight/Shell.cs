4c9ad8ca760a07e042825a08170f67fe4604b367
JamesLoo <James@JamesLoodeMac-mini.local>
2015-05-24 21:07:07 +0800

    modify ships prefeb

--- Assets/Script/Fight/Shell.cs
+++ Assets/Script/Fight/Shell.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Shell : MonoBehaviour {
 
+	Curve2D path = new Curve2D();
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +14,21 @@ public class Shell : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	public void SetShellTrack(Vector3 startPt, Vector3 endPt)
+	{
+		path.SetCubicCurve(startPt, endPt);
+	}
+
+	public void adjustArcHeight(Vector3 mid1, Vector3 mid2)
+	{
+		path.SetMiddlePoint(mid1, mid2);
+	}
+
+	public void getCubicPos(float fScale)
+	{
+		path.GetPos(fScale);
+	}
+
+
 }
--- Assets/Script/Fight/ShipFight.cs
+++ Assets/Script/Fight/ShipFight.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class ShipFight : MonoBehaviour {
+	public GameObject FightPt;
 
 	// Use this for initialization
 	void Start () {
