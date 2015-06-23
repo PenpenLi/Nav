759c75a1e29dfe28aeda2ab6abb121d119829903
wolfsq998 <wolfsq998@hotmail.com>
2015-06-24 00:47:36 +0800

    Upload it again

---
+++ Assets/Script/Building/Building.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+    public GameObject m_Sale;
+    public GameObject m_House;
+    public GameObject m_Hup;
+    public GameObject m_Progressbar;
+
+    public Sprite m_HouseIcon;
+    public Sprite m_Prog;
+
+
+    public GameObject m_Architecture;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
