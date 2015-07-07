61d626c9a08a0c9cd3ddcf307c2993dca8b2f10a
lly20000 <lly20000@gmail.com>
2015-07-08 01:13:10 +0800

    提交战船入港实现

---
+++ Assets/Script/Lobby/AchorShip.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchorShip : MonoBehaviour {
+    Vector3 m_comeBeginPos = new Vector3(-2280.0f, 1037.0f, 0.0f);
+    Vector3 m_comeEndPos = new Vector3(-545.0f, -99.0f, 0.0f);
+	// Use this for initialization
+	void Start () {
+        Come(50.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Come(float timeLength)//入港
+    {
+
+        gameObject.GetComponent<UISprite>().flip = UIBasicSprite.Flip.Horizontally;
+        gameObject.GetComponent<TweenPosition>().from = m_comeBeginPos;
+        gameObject.GetComponent<TweenPosition>().to = m_comeEndPos;
+        gameObject.GetComponent<TweenPosition>().duration = timeLength;
+        gameObject.GetComponent<TweenPosition>().enabled = true;
+    }
+
+    public void Back()//出港
+    {
+
+    }
+}
---
+++ Assets/Script/Lobby/ShipsManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShipsObject
+{
+    public int id;
+    public GameObject ship;
+}
+
+public class ShipsManager : MonoBehaviour {
+    List<ShipsObject> m_shipList = new List<ShipsObject>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
