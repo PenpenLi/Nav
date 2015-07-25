e1cbcc218c8f34e6e004241db643157a54d0db14
wolfsq998 <wolfsq998@hotmail.com>
2015-07-25 22:17:23 +0800

    Updtat: Scaling Map

--- Assets/Script/Camera/CameraDragMove.cs
+++ Assets/Script/Camera/CameraDragMove.cs
@@ -8,13 +8,13 @@ public class CameraDragMove : MonoBehaviour
     private Vector3 Origin;//移动停止坐标
     private Vector3 Diference;//摄像机当前坐标
     private Vector3 Dif;
-    private Vector3 LS;
+    private Vector3 CL;
     private bool Drag = false;//是否开始记录点击点坐标的开关
 
     float CameraSize ;  
-    const float Scalingfactor_x = 1.8f;
-    const float Scalingfactor_y = 1f;
-    const float CS = 1.5f;
+    const float Scalingfactor_x = 1.8f; // X 轴边界变化系数
+    const float Scalingfactor_y = 1f;  // Y 轴边界变化系数
+    const float CS = 1.5f; // The initial size of the camera 
 
     void Start()
     {
@@ -31,13 +31,11 @@ public class CameraDragMove : MonoBehaviour
     {
         if (Input.GetMouseButton(0)) // 鼠标左键是否按下
         {
-            CameraSize = m_camera.orthographicSize;
             Diference = (m_camera.ScreenToWorldPoint(Input.mousePosition)) - ResetCamera;//获得相机移动距离
             if (Drag == false)
             {
                 Drag = true;
                 Origin = m_camera.ScreenToWorldPoint(Input.mousePosition);
-                //Debug.Log("Origin = " + Origin);
             }
         }
         else
@@ -47,41 +45,46 @@ public class CameraDragMove : MonoBehaviour
         if (Drag == true)
         {
             ResetCamera = Origin - Diference;
-            float Csize_x = 0;// (CS - CameraSize) * Scalingfactor_x;
-            Debug.Log(Csize_x);
-            if (ResetCamera.x > 5.1 + Csize_x)
+            float Csize_x = 0;
+
+            // X 轴边界
+            if (ResetCamera.x > 4.2 + Csize_x)
             {
-                ResetCamera.x = 5.05f + Csize_x;
+                ResetCamera.x = 4.15f + Csize_x;
                 m_camera.transform.position = ResetCamera;
             }
-            else if (ResetCamera.x < -(3.9 + Csize_x))
+            else if (ResetCamera.x < -(3.0 + Csize_x))
             {
-                ResetCamera.x = -(3.85f + Csize_x);
+                ResetCamera.x = -(2.95f + Csize_x);
+                CL = m_camera.transform.localPosition;
+                CL.x= m_camera.orthographicSize * 576;
                 m_camera.transform.position = ResetCamera;
             }
-            else if (ResetCamera.x > -(3.9 + Csize_x) || ResetCamera.x < 5.1 + Csize_x)
+            else if (ResetCamera.x > -(3.0 + Csize_x) || ResetCamera.x < 4.2 + Csize_x)
             {
                 m_camera.transform.position = ResetCamera;
-                Debug.Log("ResetCamera X=" + ResetCamera);
+
             }
 
-            float Csize_y = 0;// (CS - CameraSize) * Scalingfactor_y;
-            if (ResetCamera.y > 5.7 + Csize_y)
+            // Y 轴边界
+            float Csize_y = 0;
+            if (ResetCamera.y > 5.2 + Csize_y)
             {
-                ResetCamera.y = 5.65f + Csize_y;
+                ResetCamera.y = 5.15f + Csize_y;
                 m_camera.transform.position = ResetCamera;
             }
-            else if (ResetCamera.y < -(2.4 + Csize_y))
+            else if (ResetCamera.y < -(1.9 + Csize_y))
             {
-                ResetCamera.y = -(2.35f + Csize_y);
+                ResetCamera.y = -(1.85f + Csize_y);
                 m_camera.transform.position = ResetCamera;
             }
-            else if (ResetCamera.y > -(2.4 + Csize_y) || ResetCamera.y < 5.7 + Csize_y)
+            else if (ResetCamera.y > -(1.9 + Csize_y) || ResetCamera.y < 5.2 + Csize_y)
             {
                 m_camera.transform.position = ResetCamera;
-                Debug.Log("ResetCamera Y=" + ResetCamera);
+
             }
         }
+
         //RESET CAMERA TO STARTING POSITION WITH RIGHT CLICK
         if (Input.GetMouseButton(1))// 鼠标右键是否按下
         {
--- Assets/Script/Camera/ScalingMap.cs
+++ Assets/Script/Camera/ScalingMap.cs
@@ -4,71 +4,90 @@ using System.Collections;
 
 public class ScalingMap : MonoBehaviour {
 
-    public GameObject m_Map ;
-    public Camera m_Camera;
-
+    //public Camera m_Camera;
+    public GameObject m_CameraSize;
+    public GameObject m_Leng1;
+    public GameObject m_Leng2;
+    public GameObject m_Scaling;
+    public GameObject m_camera;
     private Vector3 TFScale ; //Transform.Scale
     
-
-    //记录上一次触摸位置判断是在左放大还是缩小手势
+    //记录上一次触摸位置
      private Vector3 oldPos1;
      private Vector3 oldPos2;
 
-     const float CS = 1.5f;
+     const float MaxCS = 2.0f;
+     const float MinCS = 1.0f;
+
      float CameraSize;
 
 
      ////缩放系数
-     private float distance = 10.0f;
+     private float distance ;
 
 	// Use this for initialization
 	void Start () {
-        TFScale = m_Map.transform.localScale;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Scalingmap();
+        Scalingmap();        
 	}
     
     //ScalingMap
     void Scalingmap()
     {
-        if (Input.touchCount >1) //触摸点
+        distance = float.Parse( m_Scaling.transform.FindChild("GetSize").GetComponent<UILabel>().text);
+        CameraSize = m_camera.transform.GetComponent<Camera>().orthographicSize;
+        if (Input.touchCount > 1) //触摸点
         {
+            m_camera.GetComponent<CameraDragMove>().enabled = false;
             if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)
             {
                 //获得触点坐标
-                var Pos1 = Input.GetTouch(0).position;
-                var Pos2 = Input.GetTouch(1).position;
+                Vector3 NewPos1 = Input.GetTouch(0).position;
+                Vector3 NewPos2 = Input.GetTouch(1).position;
 
-                if (isEnlarge(oldPos1, oldPos2, oldPos1, oldPos2))//放大
+                if (isEnlarge(oldPos1, oldPos2, NewPos1, NewPos2))//放大
                 {
-                    if (m_Camera.orthographicSize > 0.5)
+                    if (CameraSize >= MinCS)
                     {
-                        m_Camera.orthographicSize -- ;
+                        CameraSize -= distance;
+                        m_camera.transform.GetComponent<Camera>().orthographicSize = CameraSize;
                     }
                 }
                 else//缩小
                 {
-                    if (m_Camera.orthographicSize < 1.5)
+                    if (CameraSize <= MaxCS)
                     {
-                        m_Camera.orthographicSize++;
+                        CameraSize += distance;
+                        m_camera.transform.GetComponent<Camera>().orthographicSize = CameraSize;
                     }
                 }
                 //备份上一次触摸点的位置，用于对比
-                oldPos1 = Pos1;
-                oldPos2 = Pos2;
- 
+                oldPos1 = NewPos1;
+                oldPos2 = NewPos2;
+
+                m_Leng1.transform.FindChild("GetSize").GetComponent<UILabel>().text = oldPos1.ToString();
+                m_Leng2.transform.FindChild("GetSize").GetComponent<UILabel>().text = NewPos1.ToString();
+
             }
         }
+        // 防止在缩放过程中有一个触摸点先松开，会读取但触摸点移动地图
+        else if (Input.touchCount == 0)
+        {
+            m_camera.GetComponent<CameraDragMove>().enabled = true;
+        }
+        m_CameraSize.transform.FindChild("GetSize").GetComponent<UILabel>().text = CameraSize.ToString();
+        
     }
 
     bool isEnlarge(Vector3 oP1,Vector3 oP2, Vector3 nP1 , Vector3 nP2)
     {
         //函数传入上一次触摸两点的位置与本次触摸两点的位置计算出用户的手势
-        var leng1 =Mathf.Sqrt((oP1.x-oP2.x)*(oP1.x-oP2.x)+(oP1.y-oP2.y)*(oP1.y-oP2.y));
-        var leng2 =Mathf.Sqrt((nP1.x-nP2.x)*(nP1.x-nP2.x)+(nP1.y-nP2.y)*(nP1.y-nP2.y));
+        float leng1 =Mathf.Sqrt((oP1.x-oP2.x)*(oP1.x-oP2.x)+(oP1.y-oP2.y)*(oP1.y-oP2.y));
+        float leng2 =Mathf.Sqrt((nP1.x-nP2.x)*(nP1.x-nP2.x)+(nP1.y-nP2.y)*(nP1.y-nP2.y));
         if(leng1<leng2)
         {
             return true;//放大
